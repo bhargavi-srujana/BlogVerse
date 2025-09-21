efdd9b600eef0a7bca6cb5949ef5d6236dd80001
Srujana <pithanisrujana@gmail.com>
2025-09-21 16:02:07 +0530

    testing

--- frontend/src/NavBar.jsx
+++ frontend/src/NavBar.jsx
@@ -8,7 +8,7 @@ const NavBar = () => {
       <Link to="/" className='text-sm md:text-lg text-gray-700 hover:bg-blue-200 px-5 py-2 rounded-lg cursor-pointer duration-200'>Home</Link>
       <div className='flex gap-2 md:gap-5 items-center'>
         <Link to="/login" className='font-semibold text-gray-700 text-xs md:text-md'>Sign in</Link>
-        <Link to="/signup" className='bg-blue-600 text-white px-1.5 py-1.5 md:px-5 md:py-2 rounded-xl text-xs md:text-md cursor-pointer'>Sign up</Link>
+        <Link to="/signup" className='bg-red-600 text-white px-1.5 py-1.5 md:px-5 md:py-2 rounded-xl text-xs md:text-md cursor-pointer'>Sign up</Link>
       </div>
     </div>
   )
